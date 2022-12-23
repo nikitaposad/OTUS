65c196d49e762f9d30a15dfc8c555157dbb50547
Nikita Korneychuk <dnevnikprogramista@gmail.com>
2022-12-23 20:18:07 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,23 @@
+
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+
+	// Reversing the string.
+	str := "Hello, OTUS!"
+
+	// returns the reversed string.
+	strRev := reverse(str)
+	fmt.Println(strRev)
+}
+
+// function, which takes a string as
+// argument and return the reverse of string.
+func reverse(str string) (result string) {
+	for _, element := range str {
+		result = string(element) + result
+	}
+	return
 }
